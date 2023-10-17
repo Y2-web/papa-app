73186130a74ea395166a51e3c52667e2bdba652c
Y2-web <emmanuel.salameh@edu.ece.fr>
2023-10-17 16:43:36 +0200

    ajout de tailwind css

---
+++ src/Components/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./HomeScreen.css"
+
+function HomeScreen() {
+    return (
+        <div className="HomeScreen">
+            <p className='text-5xl'>nouveau site</p>
+        </div>
+    )
+}
+
+export default HomeScreen
